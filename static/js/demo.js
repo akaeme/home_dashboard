b15adb262c27f466d5819e9f773b61c2fae7d1a1
Fábio Silva <oibaf3@live.com.pt>
2017-08-03 19:29:57 +0100

    test temperature sensor update

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,10 +1,13 @@
-var g = new JustGage({
-    id: "gauge",
-    value: 25,
-    min: 0,
-    max: 100,
-    title: "Temperature"
-});
+var g1;
+window.onload = function(){
+    g1 = new JustGage({
+      id: "g1",
+      value: 20,
+      min: 0,
+      max: 100,
+      label: "celsius"
+    });
+};
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function() {
     console.log('Sent message');
@@ -13,6 +16,6 @@ socket.on('connect', function() {
 });
 socket.on('values', function(data) {
     console.log(data);
-    g.refresh(data['temperature']);
+    g1.refresh(data['temperature']);
 });
 
