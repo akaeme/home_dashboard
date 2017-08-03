f64ee271c34e609d25235ec3e66dbb02f8d96e63
Fábio Silva <oibaf3@live.com.pt>
2017-08-03 20:08:31 +0100

    .

--- static/js/demo.js
+++ static/js/demo.js
@@ -1,4 +1,4 @@
-var g1;
+var g1, g2;
 window.onload = function(){
     g1 = new JustGage({
       id: "g1",
@@ -7,11 +7,28 @@ window.onload = function(){
       max: 45,
       label: "Celsius",
       levelColors: [
-          "#00fff6",
-          "#ff00fc",
-          "#1200ff"
+          "#F2B2F4",
+          "#99C7FC",
+          "#9FF2A1",
+          "#F7D274",
+          "#EF7A7A"
         ],
-      gaugeWidthScale: 0.2
+      gaugeWidthScale: 0.3
+    });
+    g2 = new JustGage({
+      id: "g2",
+      value: 0,
+      min: 0,
+      max: 100,
+      label: "Percent",
+      levelColors: [
+          "#DBCFB0",
+          "#BFC8AD",
+          "#90B494",
+          "#718F94",
+          "#545775"
+        ],
+      gaugeWidthScale: 0.3
     });
 };
 var socket = io.connect('http://' + document.domain + ':' + location.port);
@@ -21,7 +38,7 @@ socket.on('connect', function() {
     //socket.send('User has connected!');
 });
 socket.on('values', function(data) {
-    console.log(data);
     g1.refresh(data['temperature']);
+    g2.refresh(data['humidity']);
 });
 
